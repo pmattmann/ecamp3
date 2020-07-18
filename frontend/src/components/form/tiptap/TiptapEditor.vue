e6c1c3f88269805387c205c44f269729aa579d5e
Manuel Meister <news.manuelsworld@gmail.com>
2020-07-18 13:17:05 +0200

    Simplified toolbar

--- frontend/src/components/form/tiptap/TiptapEditor.vue
+++ frontend/src/components/form/tiptap/TiptapEditor.vue
@@ -13,7 +13,7 @@ import {
   Underline,
   History
 } from 'tiptap-extensions'
-import { VBtn, VIcon, VItemGroup, VSpacer, VToolbar } from 'vuetify/lib'
+import { VBtn, VIcon, VItemGroup, VSpacer, VToolbar, VTooltip, VOverflowBtn } from 'vuetify/lib'
 
 export default {
   name: 'TiptapEditor',
@@ -112,47 +112,87 @@ export default {
               }
             }, [
               this.$createElement(VToolbar, {
-                props: { short: true }
+                props: { dense: true, rounded: true },
+                staticClass: 'v-toolbar--narrow'
               }, [
-                this.$createElement(VItemGroup, {
-                  staticClass: 'v-btn-toggle v-btn-toggle--dense',
-                  props: { dense: true }
-                }, [
-                  this.genToolbarItem(props.isActive.heading({ level: 1 }), () => props.commands.heading({ level: 1 }), 'mdi-format-header-1'),
-                  this.genToolbarItem(props.isActive.heading({ level: 2 }), () => props.commands.heading({ level: 2 }), 'mdi-format-header-2'),
-                  this.genToolbarItem(props.isActive.heading({ level: 3 }), () => props.commands.heading({ level: 3 }), 'mdi-format-header-3')
-                ]),
-                this.$createElement('div', { staticClass: 'mx-1' }),
-                this.$createElement(VItemGroup, {
-                  staticClass: 'v-btn-toggle v-btn-toggle--dense',
-                  props: { dense: true, multiple: true }
-                }, [
-                  this.genToolbarItem(props.isActive.bold(), props.commands.bold, 'mdi-format-bold'),
-                  this.genToolbarItem(props.isActive.italic(), props.commands.italic, 'mdi-format-italic'),
-                  this.genToolbarItem(props.isActive.underline(), props.commands.underline, 'mdi-format-underline'),
-                  this.genToolbarItem(props.isActive.strike(), props.commands.strike, 'mdi-format-strikethrough')
-                ])
+                this.genToolbarItem(props.isActive.bold(), props.commands.bold, 'mdi-format-bold', this.$t('global.wysiwyg.bold')),
+                this.genToolbarItem(props.isActive.italic(), props.commands.italic, 'mdi-format-italic', this.$t('global.wysiwyg.italic')),
+                this.genToolbarItem(props.isActive.underline(), props.commands.underline, 'mdi-format-underline', this.$t('global.wysiwyg.underline')),
+                this.genToolbarItem(props.isActive.strike(), props.commands.strike, 'mdi-format-strikethrough', this.$t('global.wysiwyg.strike')),
+                this.$createElement(VOverflowBtn, {
+                  style: {
+                    width: '180px'
+                  },
+                  props: {
+                    hideDetails: true,
+                    text: true,
+                    dense: true,
+                    label: this.$t('global.wysiwyg.level.name'),
+                    items: [
+                      {
+                        text: this.$t('global.wysiwyg.level.1'),
+                        value: 1
+                      },
+                      {
+                        text: this.$t('global.wysiwyg.level.2'),
+                        value: 2
+                      },
+                      {
+                        text: this.$t('global.wysiwyg.level.3'),
+                        value: 3
+                      },
+                      {
+                        text: this.$t('global.wysiwyg.level.0'),
+                        value: 0
+                      }
+                    ],
+                    value: props.isActive.paragraph() ? 0 : [1, 2, 3].find(i => props.isActive.heading({ level: i }))
+                  },
+                  on: {
+                    change: (value) => {
+                      if (value === 0) {
+                        props.commands.paragraph()
+                      } else {
+                        props.commands.heading({ level: value })
+                      }
+                    }
+                  }
+                })
               ])
             ])
           }
         }
       })
     },
-    genToolbarItem (isActive, onClick, icon) {
-      return this.$createElement(VBtn, {
-        class: {
-          'v-item--active': isActive,
-          'v-btn--active': isActive
+    genToolbarItem (isActive, onClick, icon, label) {
+      return this.$createElement(VTooltip, {
+        props: {
+          top: true
         },
-        on: { click: onClick }
-      }, [
-        this.$createElement(VIcon, {}, [icon])
-      ])
+        scopedSlots: {
+          activator: ({ on, attrs }) => {
+            return this.$createElement(VBtn, {
+              staticClass: 'ml-1 px-2 editbar__btn',
+              class: {
+                'v-btn--active': isActive
+              },
+              props: {
+                text: true,
+                tile: true
+              },
+              attrs: attrs,
+              on: Object.assign(on, { click: onClick })
+            }, [
+              this.$createElement(VIcon, {}, [icon])
+            ])
+          }
+        }
+      }, label)
     },
     genEditorContent () {
       // TODO: Emit MouseDown/Up events
       return this.$createElement(EditorContent, {
-        staticClass: 'editor__content',
+        staticClass: 'editor__content pt-2',
         props: {
           editor: this.editor
         }
@@ -161,99 +201,88 @@ export default {
   },
   render (h) {
     return h('div', {
-      staticClass: 'editor'
+      staticClass: 'editor py-2'
     }, this.getContent())
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
-.v-text-field--filled:not(.v-text-field--single-line) div.editor {
-  margin-top: 10px;
-}
+  .v-text-field--filled:not(.v-text-field--single-line) div.editor {
+    margin-top: 10px;
+  }
 
-div.editor {
-  -webkit-box-flex: 1;
-  -ms-flex: 1 1 auto;
-  flex: 1 1 auto;
-  padding: 8px 0 8px;
-  max-width: 100%;
-  min-width: 0;
-  width: 100%;
-}
+  div.editor ::v-deep .ProseMirror {
+    border: 0 !important;
+    box-shadow: none !important;
+    outline: none;
+    color: rgba(0, 0, 0, 0.87);
+    line-height: normal !important;
 
-div.editor >>> .editor__content {
-  -webkit-box-flex: 1;
-  -ms-flex: 1 1 auto;
-  flex: 1 1 auto;
-  padding: 10px 0 0 0;
-}
+    h1, h2, h3, p, ol, ul {
+      margin-bottom: 6px;
+    }
 
-div.editor >>> .editor__content .ProseMirror {
-  border: 0 !important;
-  box-shadow: none !important;
-  outline: none;
-  color: rgba(0, 0, 0, 0.87);
-  line-height: normal !important;
-}
+    h1 {
+      margin-top: 18px;
+    }
 
-div.editor >>> .editor__content .ProseMirror p,
-div.editor >>> .editor__content .ProseMirror ol,
-div.editor >>> .editor__content .ProseMirror ul {
-  margin-bottom: 6px;
-}
-div.editor >>> .editor__content .ProseMirror h1 {
-  margin-top: 18px;
-  margin-bottom: 6px;
-}
-div.editor >>> .editor__content .ProseMirror h2 {
-  margin-top: 15px;
-  margin-bottom: 6px;
-}
-div.editor >>> .editor__content .ProseMirror h3 {
-  margin-top: 12px;
-  margin-bottom: 6px;
-}
-div.editor >>> .editor__content .ProseMirror :first-child {
-  margin-top: 0;
-}
-div.editor >>> .editor__content .ProseMirror li p {
-  margin-bottom: 3px;
-}
-div.editor >>> .editor__content .ProseMirror li p:not(:last-child) {
-  margin-bottom: 0;
-}
+    h2 {
+      margin-top: 15px;
+    }
 
-div.editor >>> .menububble {
-  position: absolute;
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: flex;
-  z-index: 20;
-  /*background: #000;*/
-  border-radius: 5px;
-  padding: .3rem;
-  margin-bottom: .5rem;
-  -webkit-transform: translateX(-50%);
-  transform: translateX(-50%);
-  visibility: hidden;
-  opacity: 0;
-  -webkit-transition: opacity .2s,visibility .2s;
-  transition: opacity .2s,visibility .2s;
-}
+    h3 {
+      margin-top: 12px;
+    }
 
-div.editor >>> .menububble.is-active {
-  opacity: 1;
-  visibility: visible;
-}
+    :first-child {
+      margin-top: 0;
+    }
 
-div.editor >>> .menububble__button {
-  border-radius: 3px;
-}
+    li p {
+      margin-bottom: 3px;
+    }
 
-div.editor >>> .menububble__button.is-active {
-  background-color: hsla(0, 0%, 100%, .3);
-}
+    li p:not(:last-child) {
+      margin-bottom: 0;
+    }
+  }
+
+  div.editor ::v-deep .menububble {
+    position: absolute;
+    display: flex;
+    z-index: 20;
+    margin-bottom: .5rem;
+    transform: translateX(-50%);
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity .2s, visibility .2s;
+
+    &.is-active {
+      opacity: 1;
+      visibility: visible;
+    }
+
+    .v-select__selections {
+      width: auto;
+    }
+
+    .v-select__selections input {
+      min-width: 0;
+    }
+
+    .v-overflow-btn .v-input__slot {
+      border: none;
+    }
+  }
+
+  div.editor ::v-deep .v-toolbar--narrow .v-toolbar__content {
+    padding: 0;
+  }
+
+  div.editor ::v-deep .editbar__btn {
+    min-width: 48px;
+  }
 
 </style>
